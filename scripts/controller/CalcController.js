72cd87f7200d550b048f416f8ffe9a5ad2c257c3
João Rangel <joaohcrangel@gmail.com>
2019-09-09 17:59:50 -0300

    C16 - Correções de Bugs com o Ponto

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -198,7 +198,7 @@ class CalcController {
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
 
@@ -217,6 +217,8 @@ class CalcController {
     addDot() {
 
         let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
         if (this.isOperator(lastOperation) || !lastOperation) {
             this.pushOperation('0.');
