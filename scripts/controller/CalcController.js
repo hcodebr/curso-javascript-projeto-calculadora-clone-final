021a7de10205ab24cd391762185f8f21714d79dd
João Rangel <joaohcrangel@gmail.com>
2019-09-09 16:29:06 -0300

    C03 - Método Construtor e Encapsulamento

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,5 +1,30 @@
 class CalcController {
 
-    
+    constructor() {
+
+        this._displayCalc = "0";
+        this._dataAtual; 
+
+    }
+
+    get displayCalc() {
+
+        return this._displayCalc;
+
+    }
+
+    set displayCalc(valor) {
+
+        this._displayCalc = valor;
+        
+    }
+
+    get dataAtual() {
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor) {
+        this._dataAtual = valor;
+    }
 
 }
