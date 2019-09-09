512e4db6e9a8b644a23f243453f2d2f1771c3735
João Rangel <joaohcrangel@gmail.com>
2019-09-09 18:11:27 -0300

    C18 - Ctrl + c e Ctrl + v

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -19,6 +19,34 @@ class CalcController {
 
     }
 
+    copyToClipboard() {
+
+        let input = document.createElement('input');
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
+
+    }
+
+    pasteFromClipboard() {
+
+        document.addEventListener('paste', e => {
+
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text);
+
+        });
+
+    }
+
     initialize() {
 
         this.setDisplayDateTime();
@@ -30,6 +58,7 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
 
     }
 
@@ -70,6 +99,10 @@ class CalcController {
                 case '8':
                 case '9':
                     this.addOperation(parseInt(e.key));
+                    break;
+
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
                     break;
                 
             }
