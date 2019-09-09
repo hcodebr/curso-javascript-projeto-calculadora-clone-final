0509dbedaf800906ef879b3c7a782b1fb0a984f6
João Rangel <joaohcrangel@gmail.com>
2019-09-09 17:13:06 -0300

    C10 - Adicionando operações - isNaN e Array.length

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -49,9 +49,49 @@ class CalcController {
 
     }
 
+    getLastOperation() {
+
+        return this._operation[this._operation.length - 1];
+
+    }
+
+    setLastOperation(value) {
+
+        this._operation[this._operation.length - 1] = value;
+
+    }
+
+    isOperator(value) {
+
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+
+    }
+
     addOperation(value) {
 
-        this._operation.push(value);
+        if (isNaN(this.getLastOperation())) {
+
+            if (this.isOperator(value)) {
+
+                this.setLastOperation(value);
+
+            } else if(isNaN(value)) {
+
+                //Outra coisa
+                console.log('value');
+
+            } else {
+
+                this._operation.push(value);
+
+            }
+
+        } else {
+
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
+
         console.log(this._operation);
 
     }
@@ -72,22 +112,25 @@ class CalcController {
                 this.clearEntry();
                 break;
             case 'soma':
-
+                this.addOperation('+');
                 break;
             case 'subtracao':
-
+                this.addOperation('-');
                 break;
             case 'divisao':
-
+                this.addOperation('/');
                 break;
             case 'multiplicacao':
-
+                this.addOperation('*');
                 break;
             case 'porcento':
-
+                this.addOperation('%');
                 break;
             case 'igual':
-
+                
+                break;
+            case 'ponto':
+                this.addOperation('.');
                 break;
             case '0':
             case '1':
