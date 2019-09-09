a6f98defa3089794381e125bebf367e6c17a3403
João Rangel <joaohcrangel@gmail.com>
2019-09-09 18:04:58 -0300

    C17 - Eventos de Teclado

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -15,6 +15,7 @@ class CalcController {
 
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
 
     }
 
@@ -29,6 +30,51 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+
+    }
+
+    initKeyboard() {
+
+        document.addEventListener('keyup', e => {
+
+            switch (e.key) {
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+                
+            }
+
+        });
 
     }
 
