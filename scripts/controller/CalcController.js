3101940281d5b4d096e1d8429a148defbc1306a1
João Rangel <joaohcrangel@gmail.com>
2019-09-09 17:37:14 -0300

    C13 - Botão Porcento

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -25,6 +25,8 @@ class CalcController {
 
         }, 1000);
 
+        this.setLastNumberToDisplay();
+
     }
 
     addEventListenerAll(element, events, fn) {
@@ -41,11 +43,15 @@ class CalcController {
 
         this._operation = [];
 
+        this.setLastNumberToDisplay();
+
     }
 
     clearEntry() {
 
         this._operation.pop();
+
+        this.setLastNumberToDisplay();
 
     }
 
@@ -81,13 +87,26 @@ class CalcController {
 
     calc() {
 
-        let last = this._operation.pop();
+        let last = '';
+        
+        if (this._operation.length > 3) {
+            last = this._operation.pop();
+        }
 
         let result = eval(this._operation.join(""));
 
-        this._operation = [result, last];
+        if (last === '%') {
 
-        console.log(this._operation);
+            result /= 100;
+            this._operation = [result];
+
+        } else {
+
+            this._operation = [result];
+
+            if (last) this._operation.push(last);
+
+        }
 
         this.setLastNumberToDisplay();
 
@@ -106,7 +125,7 @@ class CalcController {
 
         }
 
-        console.log('lastNumber', lastNumber);
+        if (!lastNumber) lastNumber = 0;
 
         this.displayCalc = lastNumber;
 
@@ -183,7 +202,7 @@ class CalcController {
                 this.addOperation('%');
                 break;
             case 'igual':
-                
+                this.calc();
                 break;
             case 'ponto':
                 this.addOperation('.');
