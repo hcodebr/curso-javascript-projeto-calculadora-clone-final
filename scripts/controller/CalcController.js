488daae9e18ef53b3ddbbeb9727c7ebec0dccc7e
João Rangel <joaohcrangel@gmail.com>
2019-09-09 18:20:46 -0300

    C21 - Tratar outros erros no eval() com Try Catch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -202,7 +202,11 @@ class CalcController {
 
     getResult() {
 
-        return eval(this._operation.join(""));
+        try {
+            return eval(this._operation.join(""));
+        } catch (e) {
+            setTimeout(() => this.setError(), 1);
+        }
 
     }
 
